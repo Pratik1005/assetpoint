8f125da0f224ba22dfa51aa7778f9ea3fac6f43a
Pratik Devle <pratik.devle98@gmail.com>
2022-04-07 20:19:15 +0530

    feat: added simple auth

--- src/components/AddressForm.jsx
+++ src/components/AddressForm.jsx
@@ -28,178 +28,176 @@ const AddressForm = ({
 
   return (
     <section className="address-modal">
-      <div className="form-ctn form-space">
-        <form>
-          <h3 className="text-center mg-bottom-md">Address</h3>
-          <div className="form-control">
-            <label htmlFor="country" className="fw-bold">
-              Country
-            </label>
-            <select
-              name="countries"
-              value={addressData.country}
-              onChange={(e) =>
-                setAddressData({...addressData, country: e.target.value})
-              }
-            >
-              <option value="india">India</option>
-              <option value="canada">Canada</option>
-              <option value="germany">Germany</option>
-            </select>
-          </div>
-          <div className="form-control">
-            <label htmlFor="full-name" className="fw-bold">
-              Full name
-            </label>
-            <input
-              type="text"
-              name="full-name"
-              id="full-name"
-              placeholder="Firstname Lastname"
-              required
-              value={addressData.name}
-              onChange={(e) =>
-                setAddressData({...addressData, name: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="contact" className="fw-bold">
-              Contact
-            </label>
-            <input
-              type="text"
-              name="contact"
-              id="contact"
-              placeholder="9999999999"
-              required
-              value={addressData.contact}
-              onChange={(e) =>
-                setAddressData({...addressData, contact: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="pin-code" className="fw-bold">
-              Pin code
-            </label>
-            <input
-              type="text"
-              name="pin-code"
-              id="pin-mobile"
-              placeholder="400012"
-              required
-              value={addressData.pinCode}
-              onChange={(e) =>
-                setAddressData({...addressData, pinCode: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="flat-name" className="fw-bold">
-              Flat, House no., Building
-            </label>
-            <input
-              type="text"
-              name="flat-name"
-              id="flat-name"
-              placeholder="101 Nirmal House"
-              required
-              value={addressData.flatName}
-              onChange={(e) =>
-                setAddressData({...addressData, flatName: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="area" className="fw-bold">
-              Area, Colony, Street, Sector
-            </label>
-            <input
-              type="text"
-              name="area"
-              id="area"
-              placeholder="Area 1"
-              required
-              value={addressData.area}
-              onChange={(e) =>
-                setAddressData({...addressData, area: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="land-mark" className="fw-bold">
-              Landmark
-            </label>
-            <input
-              type="text"
-              name="land-mark"
-              id="land-mark"
-              placeholder="ITC hotel"
-              required
-              value={addressData.landMark}
-              onChange={(e) =>
-                setAddressData({...addressData, landMark: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="area" className="fw-bold">
-              City
-            </label>
-            <input
-              type="text"
-              name="city"
-              id="city"
-              placeholder="city"
-              required
-              value={addressData.city}
-              onChange={(e) =>
-                setAddressData({...addressData, city: e.target.value})
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="area" className="fw-bold">
-              State
-            </label>
-            <select
-              name="states"
-              value={addressData.state}
-              onChange={(e) =>
-                setAddressData({...addressData, state: e.target.value})
-              }
-            >
-              <option value="maharashtra">Maharashtra</option>
-              <option value="delhi">Delhi</option>
-              <option value="karnataka">Karnataka</option>
-            </select>
-          </div>
-          <div className="form-control form-option">
-            {isEditMode ? (
-              <button
-                className="btn btn-primary"
-                onClick={(e) => handleAddressForm(e)}
-              >
-                Save address
-              </button>
-            ) : (
-              <button
-                className="btn btn-primary"
-                onClick={(e) => handleAddressForm(e)}
-              >
-                Add address
-              </button>
-            )}
+      <form className="br-md form-space">
+        <h3 className="text-center mg-bottom-md">Address</h3>
+        <div className="form-control">
+          <label htmlFor="country" className="fw-bold">
+            Country
+          </label>
+          <select
+            name="countries"
+            value={addressData.country}
+            onChange={(e) =>
+              setAddressData({...addressData, country: e.target.value})
+            }
+          >
+            <option value="india">India</option>
+            <option value="canada">Canada</option>
+            <option value="germany">Germany</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label htmlFor="full-name" className="fw-bold">
+            Full name
+          </label>
+          <input
+            type="text"
+            name="full-name"
+            id="full-name"
+            placeholder="Firstname Lastname"
+            required
+            value={addressData.name}
+            onChange={(e) =>
+              setAddressData({...addressData, name: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="contact" className="fw-bold">
+            Contact
+          </label>
+          <input
+            type="text"
+            name="contact"
+            id="contact"
+            placeholder="9999999999"
+            required
+            value={addressData.contact}
+            onChange={(e) =>
+              setAddressData({...addressData, contact: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="pin-code" className="fw-bold">
+            Pin code
+          </label>
+          <input
+            type="text"
+            name="pin-code"
+            id="pin-mobile"
+            placeholder="400012"
+            required
+            value={addressData.pinCode}
+            onChange={(e) =>
+              setAddressData({...addressData, pinCode: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="flat-name" className="fw-bold">
+            Flat, House no., Building
+          </label>
+          <input
+            type="text"
+            name="flat-name"
+            id="flat-name"
+            placeholder="101 Nirmal House"
+            required
+            value={addressData.flatName}
+            onChange={(e) =>
+              setAddressData({...addressData, flatName: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="area" className="fw-bold">
+            Area, Colony, Street, Sector
+          </label>
+          <input
+            type="text"
+            name="area"
+            id="area"
+            placeholder="Area 1"
+            required
+            value={addressData.area}
+            onChange={(e) =>
+              setAddressData({...addressData, area: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="land-mark" className="fw-bold">
+            Landmark
+          </label>
+          <input
+            type="text"
+            name="land-mark"
+            id="land-mark"
+            placeholder="ITC hotel"
+            required
+            value={addressData.landMark}
+            onChange={(e) =>
+              setAddressData({...addressData, landMark: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="area" className="fw-bold">
+            City
+          </label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="city"
+            required
+            value={addressData.city}
+            onChange={(e) =>
+              setAddressData({...addressData, city: e.target.value})
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="area" className="fw-bold">
+            State
+          </label>
+          <select
+            name="states"
+            value={addressData.state}
+            onChange={(e) =>
+              setAddressData({...addressData, state: e.target.value})
+            }
+          >
+            <option value="maharashtra">Maharashtra</option>
+            <option value="delhi">Delhi</option>
+            <option value="karnataka">Karnataka</option>
+          </select>
+        </div>
+        <div className="form-control form-option">
+          {isEditMode ? (
             <button
-              className="btn btn-icon-text-outline"
-              onClick={handleCancelAddress}
+              className="btn btn-primary"
+              onClick={(e) => handleAddressForm(e)}
             >
-              Cancel
+              Save address
             </button>
-          </div>
-        </form>
-      </div>
+          ) : (
+            <button
+              className="btn btn-primary"
+              onClick={(e) => handleAddressForm(e)}
+            >
+              Add address
+            </button>
+          )}
+          <button
+            className="btn btn-icon-text-outline"
+            onClick={handleCancelAddress}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
     </section>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,108 @@
 import "../styles/auth.css";
-import {NavMenu} from "../components/NavMenu";
-import {Link} from "react-router-dom";
+import {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {NavMenu, Footer} from "../components/allComponents";
+
 const Login = () => {
-    return (
-        <>
-        <NavMenu />
-         <section className="form-ctn br-md">
-        <form>
-            <h2 className="text-center mg-bottom-md">Login</h2>
-            <div className="form-control">
-                <label htmlFor="email" className="fw-bold">Email address</label>
-                <input type="email" name="email" id="email" placeholder="name@gmail.com" />
-            </div>
-            <div className="form-control">
-                <label htmlFor="password" className="fw-bold">Password</label>
-                <input type="password" name="password" id="password" placeholder="&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;" />
-            </div>
-            <div className="form-control">
-                <input type="checkbox" id="remember-me" name="checkbox" />
-                <label htmlFor="remember-me" className="fw-bold">Remember me <Link to="/forgotpassword" className="forgot-pw">Forgot your Password?</Link></label>
-            </div>
-            <div className="form-control">
-                <button className="btn btn-primary">Login</button>
-            </div>
-            <div className="account-toggle fw-bold">
-                <Link to="/signup">
-                    Create New Account <span className="material-icons fw-bold">chevron_right</span>
-                </Link>
-            </div>
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+    error: false,
+  });
+
+  const handleEmail = (e) => {
+    setLoginData((prev) => ({...prev, email: e.target.value, error: false}));
+  };
+
+  const handlePassword = (e) => {
+    setLoginData((prev) => ({...prev, password: e.target.value, error: false}));
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (
+      loginData.email === "test@gmail.com" &&
+      loginData.password === "test@123"
+    ) {
+      navigate("/account");
+    } else {
+      setLoginData({email: "", password: "", error: true});
+    }
+  };
+
+  const handleTestLogin = (e) => {
+    e.preventDefault();
+    setLoginData({email: "test@gmail.com", password: "test@123", error: false});
+  };
+  return (
+    <>
+      <NavMenu />
+      <section className="app-ctn">
+        <form className="br-md">
+          <h2 className="text-center mg-bottom-md">Login</h2>
+          {loginData.error && (
+            <p className="error-msg form-control">Invalid credentials</p>
+          )}
+          <div className="form-control">
+            <label htmlFor="email" className="fw-bold">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="name@gmail.com"
+              value={loginData.email}
+              onChange={(e) => handleEmail(e)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password" className="fw-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={loginData.password}
+              onChange={(e) => handlePassword(e)}
+              placeholder="&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;"
+            />
+          </div>
+          <div className="form-control">
+            <input type="checkbox" id="remember-me" name="checkbox" />
+            <label htmlFor="remember-me" className="fw-bold">
+              Remember me{" "}
+            </label>
+            <Link to="/forgotpassword" className="forgot-pw fw-bold">
+              Forgot your Password?
+            </Link>
+          </div>
+          <div className="form-control">
+            <button className="btn btn-primary" onClick={(e) => handleLogin(e)}>
+              Login
+            </button>
+          </div>
+          <div className="form-control">
+            <button
+              className="btn btn-primary-outline"
+              onClick={(e) => handleTestLogin(e)}
+            >
+              Login with test credentials
+            </button>
+          </div>
+          <div className="account-toggle fw-bold">
+            <Link to="/signup">
+              Create New Account{" "}
+              <span className="material-icons fw-bold">chevron_right</span>
+            </Link>
+          </div>
         </form>
-    </section>
-        </>
-    );
-}
+      </section>
+      <Footer />
+    </>
+  );
+};
 
 export {Login};
