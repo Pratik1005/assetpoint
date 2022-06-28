d18cd2b9be1e249cfccae9f7ef99eca96c0d190b
Pratik Devle <pratik.devle98@gmail.com>
2022-06-28 20:13:44 +0530

    fix: location was giving undefined

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useNavigate, useLocation} from "react-router-dom";
 import {useAuth, useUser} from "../context";
 import {addToWishlist, removeFromWishlist, addToCart} from "../services";
 import {isProductInCart, isProductInWishList} from "../utils/allUtils";
@@ -7,6 +7,7 @@ const ProductCard = ({productData}) => {
   const {auth} = useAuth();
   const {userDispatch, userState} = useUser();
   const navigate = useNavigate();
+  const location = useLocation();
 
   let currentItemInCart = isProductInCart(productData._id, userState.cart);
 
@@ -14,7 +15,7 @@ const ProductCard = ({productData}) => {
     if (auth.isLoggedIn) {
       addToCart(productData, auth.token, userDispatch);
     } else {
-      navigate("/login");
+      navigate("/login", {replace: true, state: {from: location}});
     }
   };
 
@@ -28,7 +29,7 @@ const ProductCard = ({productData}) => {
         addToWishlist(productData, auth.token, userDispatch);
       }
     } else {
-      navigate("/login");
+      navigate("/login", {replace: true, state: {from: location}});
     }
   };
   return (
