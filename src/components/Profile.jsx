2c8e5509f822d5fe7b3491a1b37c6da55d11baca
Pratik Devle <pratik.devle98@gmail.com>
2022-07-05 19:45:08 +0530

    refactor: profile data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,29 @@
+import {useState, useEffect} from "react";
+
 const Profile = () => {
-    return (
-        <>
-        <h3 className="text-center pd-bottom-md">Profile</h3>
-        <div className="pd-bottom-lg">
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    setUserData(JSON.parse(localStorage.getItem("userData")));
+  }, []);
+
+  return (
+    <>
+      <h3 className="text-center pd-bottom-md">Profile</h3>
+      <div className="pd-bottom-lg">
         <p>Name</p>
-        <p className="para-md">Pratik Devle</p>
-        </div>
-        <div className="pd-bottom-lg">
+        <p className="para-md">{`${userData.firstName} ${userData.lastName}`}</p>
+      </div>
+      <div className="pd-bottom-lg">
         <p>Email</p>
-        <p className="para-md">pratikdevle@gmail.com</p>
-        </div>
-        <div className="pd-bottom-lg">
+        <p className="para-md">{`${userData.email}`}</p>
+      </div>
+      <div className="pd-bottom-lg">
         <p>Contact</p>
         <p className="para-md">9876543210</p>
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
-export {Profile}
+export {Profile};
